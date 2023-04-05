dc7d7a85348c42b274e5a45526e340b6ca742eb8
Salahub (WSL) <cdsalahub@gmail.com>
2023-04-05 09:46:51 -0400

    Updated functionality

--- pkg/R/genomeHelpers.R
+++ pkg/R/genomeHelpers.R
@@ -1,51 +1,60 @@
 ### Helper Functions for Genome Objects ##############################
 
-##' @title A helper to generate a genetic sequence
+##' @title Generate a genetic sequence
 ##' @param nchrom the integer number of chromosomes in the sequence
 ##' @param nmark a vector of integers of length nchrom giving the
 ##' number of markers for each chromosome
 ##' @param alleles (optional) vector of possible encodings for the
-##' markers
+##' markers, assumed to be of length 2
 ##' @param markerFun a function which accepts an integer and vector
 ##' of alleles and returns a 2-column matrix of marker measurements
 ##' for a single chromosome to be mapped on all chromosomes
 ##' @return a list of 2-column matrices giving marker encodings on
 ##' each chromosome
 ##' @author Chris Salahub
-generateEncoding <- function(nchrom, nmark, alleles = c(1, 1),
+generateEncoding <- function(nchrom, nmark, alleles = c(1,0),
                              markerFun = markerPure) {
     ## map the single chromosome function across all chromosomes
     lapply(1:nchrom, function(ind) markerFun(nmark[ind], alleles))
 }
 
-##' @title Marker measurement generation functions
+##' @title Helpers to simulate marker measurements
 ##' @param nmark an integer giving the number of markers
-##' @param alleles (optional) vector of possible marker measurements
+##' @param alleles vector of possible marker measurements
 ##' @return a 2-column matrix with nmark rows with entries from
 ##' alleles
 ##' @author Chris Salahub
-markerPure <- function(nmark, alleles) { # pure single chromosome
-    matrix(rep(allele, nmark*2), ncol = 2)
+markerPure <- function(nmark, alleles) {
+    matrix(rep(alleles[1], nmark*2), ncol = 2)
 }
-markerHybrid <- function(nmark, alleles) { # mixed (i.e. Aa on all)
-    cbind(rep(1, nmark), rep(0, nmark))
+markerHybrid <- function(nmark, alleles) {
+    cbind(rep(alleles[1], nmark), rep(alleles[2], nmark))
 }
-markerIntercross <- function(nmark, alleles) { # from an intercross
-    allele <- as.numeric(runif(2) < 0.5)
-    cbind(rep(allele[1], nmark), rep(allele[2], nmark))
+markerIntercross <- function(nmark, alleles) {
+    cbind(rep(allele[1], nmark),
+          rep(allele[2], nmark))[,sample(1:2, 2)]
 }
-abio.back <- function(nmark, alleles) { # no drift
-    cbind(rep(allele, nmark), rep(runif(1) < 0.5, nmark))
+markerBackcross <- function(nmark, alleles) {
+    cbind(rep(allele, nmark),
+          rep(alleles[sample(1:2, 1)], nmark))
 }
-abio.random <- function(nmark, alleles) { # totally random genome
-    matrix(as.numeric(runif(2*nmark) < 0.5), ncol = 2)
+markerRandom <- function(nmark, alleles){
+    matrix(sample(alleles, size = 2*nmark, replace = TRUE), ncol = 2)
 }
 
-
-## generates distances using the specified generation function
-distGenesis <- function(nchrom, nmark, distFun) {
-    lapply(1:nchrom, function(ind) distFun(nmark[ind]-1))
+##' @title Generate distances for a genetic sequence
+##' @param nchrom integer number of chromosomes in the sequence
+##' @param nmark vector of integers of length nchrom giving the number
+##' of markers on each chromosome
+##' @param distFun function which accepts an integer and returns a
+##' vector of floats of length on less than the integer
+##' @author Chris Salahub
+distGenesis <- function(nchrom, nmark, distFun, ...) {
+    lapply(1:nchrom, function(ind) distFun(nmark[ind]-1, ...))
 }
+
+##' @title Helpers for simulating genetic distances
+
 
 ## haldane's map function
 mapHaldane <- function(d) {
